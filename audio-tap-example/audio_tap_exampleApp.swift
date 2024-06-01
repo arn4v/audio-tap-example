736901089fe8589770fc05fff8ba54f60dfdac80
Devin Gould <djgould0628@gmail.com>
2024-06-01 13:42:31 -0400

    Initial Commit

---
+++ audio-tap-example/audio_tap_exampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  audio_tap_exampleApp.swift
+//  audio-tap-example
+//
+//  Created by Devin Gould on 6/1/24.
+//
+
+import SwiftUI
+
+@main
+struct audio_tap_exampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
